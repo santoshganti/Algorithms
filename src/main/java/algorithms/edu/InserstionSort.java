06a0d3ee12d5a3193494dc972f310989ebe54271
Santosh <santosh.ganti@me.com>
2014-12-04 17:22:23 +0530

    1.Added class files for Bubble Sort, Insertion Sort, Merge Sort, Quick Sort and Selection Sort (files for Bubble, merge, quick and selction are blank and will be filled code soon)
    2. Changed Insertion sort class name to Camel Case i.e "InsertionSort"
    
    3. Changed the test class name to "TestSort" because all the test cases
    for all the sort algorithms are written in this.

---
+++ src/main/java/algorithms/edu/InserstionSort.java
@@ -0,0 +1,21 @@
+package algorithms.edu;
+
+public class InserstionSort {
+
+	public int[] inserstionSort(int inputarray[]) {
+
+		for (int j = 1; j < inputarray.length; j++) {
+			int key = inputarray[j];
+			int i = j - 1;
+
+			while ((i > -1) && (inputarray[i] > key)) {
+				inputarray[i + 1] = inputarray[i];
+				i--;
+			}
+			inputarray[i + 1] = key;
+
+		}
+
+		return inputarray;
+	}
+}
--- src/test/java/algorithms/edu/TestSort.java
+++ src/test/java/algorithms/edu/TestSort.java
@@ -5,18 +5,18 @@ import static org.junit.Assert.*;
 
 //import static junit.framework.Assert.*;
 
-public class Insertiontest {
+public class TestSort {
 
 	@Test
 	public void testSort() {
 		// Test case 1
 		int toSort[] = { 5, 2, 4, 6, 1, 3 };
 		int expected[] = { 1, 2, 3, 4, 5, 6 };
-		Inserstionsort sort = new Inserstionsort();
+		InserstionSort sort = new InserstionSort();
 
 		int[] sorted = sort.inserstionSort(toSort);
 
-		assertArrayEquals(sorted, expected);
+		assertArrayEquals("The array is not sorted properly",sorted, expected);
 
 	}
 }
